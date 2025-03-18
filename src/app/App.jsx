bc1660569fbeb775a325dce63c03dba3a593c99d
sejdigashi <2003sejdi@gmail.com>
2025-03-18 15:10:55 +0100

    first commit

---
+++ src/app/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import './../App.css';
+import './../index.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>Food Waste Reduction</p>
+    </div>
+  );
+}
+
+export default App;
