1cd344bff51be1c780e3005baddd2f3a451e022d
RedonBerisha <berisharedon10@gmail.com>
2025-03-24 00:44:32 +0100

    Input tailwind changes

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,7 +1,7 @@
 const Input = ({ className, ...props }) => {
   return (
     <input
-      className={`shadow-sm px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 ${className}`}
+      className={`shadow-sm px-3 py-2 border rounded-md focus:outline-none focus:border-orange-500 transition-colors ${className}`}
       {...props}
     />
   );
