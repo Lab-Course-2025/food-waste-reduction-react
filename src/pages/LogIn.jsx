5288fc8dcd7183a86ff66707438cf713e8c925c6
RedonBerisha <berisharedon10@gmail.com>
2025-03-26 09:28:23 +0100

    EyeOff change

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -54,7 +54,7 @@ function LogIn() {
                   onClick={() => setShowPassword(!showPassword)}
                   className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700 cursor-pointer"
                 >
-                  {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
+                  {showPassword ? <Eye size={20} /> : <EyeOff size={20} />}
                 </button>
               </div>
             </div>
