154ff9de56c3d332e5bb5fb3386ad6a447ad1ab4
sejdigashi <2003sejdi@gmail.com>
2025-03-29 16:34:23 +0100

    fix conflicts

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,13 +1,34 @@
-import { useState } from 'react';
-import './../App.css';
-import './../index.css';
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import "./index.css";
+import ScrollToTop from "./components/ScrollToTop";
+//import Header from "./components/Header"
+import Landing from "./pages/Landing";
+import Information from "./pages/Information";
+import Donors from "./pages/donors";
+import Recipient from "./pages/Recipient";
+import ContactUs from "./pages/ContactUs";
+import LogIn from "./pages/LogIn";
+import Submit from "./pages/SubmitDonation";
+import DonacionetAktive from "./pages/DonacionetAktive";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div>
-      <p>Food Waste Reduction!</p>
+      {/* <Header /> */}
+      <ScrollToTop />
+      <main>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/information" element={<Information />} />
+          <Route path="/donors" element={<Donors />} />
+          <Route path="/recipient" element={<Recipient />} />
+          <Route path="/contact-us" element={<ContactUs />} />
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/submit" element={<Submit />} />
+          <Route path="/donacionetaktive" element={<DonacionetAktive />} />
+        </Routes>
+      </main>
     </div>
   );
 }
